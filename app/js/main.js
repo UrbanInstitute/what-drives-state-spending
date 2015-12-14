2672ac458cfc1761700fd3dbd9c8ba6da21d7883
Ben Chartoff <bchartoff@gmail.com>
2015-12-14 16:38:51 -0500

    skeleton of tables

---
+++ app/js/main.js
@@ -0,0 +1,119 @@
+
+var COLUMNS = {
+	"higher": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"corrections": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"medicaid": ["spending","demographics","eligibility","takeup","spending"],
+	"ssi": ["spending","demographics","eligibility","takeup","spending"],
+	"tanf": ["spending","demographics","eligibility","takeup","spending"],
+	"ccdf": ["spending","demographics","eligibility","takeup","units","spending"],
+	"housing": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	 // "admin": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"resources": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"parks": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"utilities": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"k12": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"fire": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"police": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"highway": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"],
+	"transit": ["spending","demographics","eligibility","takeup","units","payroll","nonpayroll"]
+}
+var ROW_HEIGHT = 20;
+var headerHeight = 30;
+
+function drawMenu(){
+	var menu = d3.select("#navMenu")
+	for(var category in COLUMNS){
+		menu.append("div")
+			.attr("class", "navButton " + category)
+			.datum({ "category" : category})
+			.text(category)
+			.on("click", function(d){
+				d3.selectAll(".navButton.active").classed("active",false)
+				d3.select(this).classed("active",true)
+				return renderHeatmap(d.category);
+			})
+	}
+
+}
+function renderHeatmap(category){
+	d3.selectAll(".cell").remove();
+	d3.selectAll(".headercell").remove();
+	d3.csv("data/" + category + ".csv", function(data){
+		var heatmap = d3.select("#heatmap")
+		var header = heatmap.append("div")
+			.attr("class", "header")
+		header.append("div")
+				.attr("class","state headerCell")
+				.text("state")
+		COLUMNS[category].map(function(column){
+			header.append("div")
+				.attr("class",function(){ return column + " headerCell"})
+				.text(function(){ return column})
+				.on("click", function(){
+					var category = d3.select(this).attr("class").replace(" headerCell","")
+					var duration = 1000;
+					d3.selectAll(".row")
+						.transition()
+						.duration(duration)
+						.delay(function(d){
+							var rank = (d[category + "_rank"] == 99) ? 52:d[category + "_rank"]
+							return rank/52 * duration
+						})
+						.style("top",function(d){
+							var rank = (d[category + "_rank"] == 99) ? 52:d[category + "_rank"]
+							return ((rank * ROW_HEIGHT) + headerHeight)  +"px"
+						})
+				})
+		})
+
+		var row = heatmap.selectAll("row")
+			.data(data)
+			.enter()
+			.append("div")
+				.attr("class", function(d){
+					return "row " + d.state 
+				})
+			.style("top", function(d){
+				return ((d.spending_rank * ROW_HEIGHT) + headerHeight)  +"px"
+			})
+		row.append("div")
+			.classed("cell state", true)
+			.text(function(d){ return d.state})
+		COLUMNS[category].map(function(column){
+			row.append("div")
+				.attr("class", function(d){
+					return "cell " + column + " " + getCellClass(d, column, category)
+				})
+				.text(function(d){
+					if(d[column + "_rank"] == ""){
+						return "."
+					}
+					else if(d[column + "_rank"] == 99){
+						return "Missing!"
+					}else{ return d[column + "_rank"] }
+				})
+				.on("mouseover", function(d){ mouseover(d, column, category)})
+		})
+
+	})
+}
+
+function mouseover(datum, column, category){
+	console.log(datum[column + "_value"])
+	var temp = d3.format(".3f")
+	d3.select("#temp span").text(temp(datum[column + "_value"]))
+}
+
+function getCellClass(datum, column, category){
+	if(datum[column + "_rank"] == ""){
+		return "blankCell"
+	}
+	else if (datum[column + "_rank"] == 99){
+		return "errorCell"
+	}else{ return "decile-" + (Math.floor(datum[column + "_rank"]/5)+1) }
+}
+
+
+drawMenu();
+renderHeatmap("higher");
+d3.select(".navButton.higher").classed("active",true)
---
+++ app/scraper/build_data.py
@@ -0,0 +1,116 @@
+import csv
+import xlrd
+from operator import itemgetter
+
+def collect(l, index):
+   return map(itemgetter(index), l)
+
+BASE_PATH = "/Users/bchartof/Box Sync/TPC_StateSpendingDrivers"
+FILENAMES = {
+	"higher":"higher/Data_higher_2012.xlsx",
+	"corrections":"saftey/Corrections/Data_corrections_2012.xlsx",
+	"medicaid": "health/Data_medicaid_2012.xlsx",
+	"ssi": "social/Data_Public Welfare (SSI)_2012.xlsx",
+	"tanf": "social/Data_Public Welfare (TANF - CCDF - SSBG)_2012.xlsx",
+	"ccdf": "social/Data_Public Welfare (TANF - CCDF - SSBG)_2012.xlsx",
+	"housing": "env/housing/Data_housing_2012.xlsx",
+	# "admin": "admin/Data_admin_2012.xlsx",
+	"resources": "env/natural resources/Data_resource_2012.xlsx",
+	"parks": "env/parks/Data_parks_2012.xlsx",
+	"utilities": "env/utilities/Data_utilities_2012.xlsx",
+	"k12": "k12/Data_k12_2012.xlsx",
+	"fire": "saftey/Fire/Data_fire_2012.xlsx",
+	"police": "saftey/Police/Data_police_2012.xlsx",
+	"highway": "transport/Highway/Data_highway_2012.xlsx",
+	"transit": "transport/Transit/Data_transit_2012.xlsx"
+}
+TABNAMES = {
+	"higher": "Ratios - Current",
+	"corrections": "Ratios - Current",
+	"medicaid": "Ratios - Total",
+	"ssi": "Ratios - Total",
+	"tanf": "Ratios - TANF",
+	"ccdf": "Ratios - CCDF",
+	"housing": "Ratios - Current",
+	# "admin": "Ratios - Current",
+	"resources": "Ratios - Current",
+	"parks": "Ratios - Current",
+	"utilities": "Ratios - Current",
+	"k12": "Ratios - Current",
+	"fire": "Ratios - Current",
+	"police": "Ratios - Current",
+	"highway": "Ratios - Current",
+	"transit": "Ratios - Current"
+}
+STATES = {"MT": "Montana","WY": "Wyoming","NE": "Nebraska","ME": "Maine","ID": "Idaho","TN": "Tennessee","IN": "Indiana","MS": "Mississippi","OK": "Oklahoma","ND": "North Dakota","KY": "Kentucky","MO": "Missouri","AR": "Arkansas","WV": "West Virginia","SC": "South Carolina","HI": "Hawaii","NC": "North Carolina","LA": "Louisiana","WA": "Washington","AL": "Alabama","IL": "Illinois","KS": "Kansas","GA": "Georgia","UT": "Utah","OH": "Ohio","AZ": "Arizona","SD": "South Dakota","FL": "Florida","WI": "Wisconsin","VA": "Virginia","NH": "New Hampshire","NM": "New Mexico","DE": "Delaware","MA": "Massachusetts","TX": "Texas","RI": "Rhode Island","US": "United States","MI": "Michigan","VT": "Vermont","CO": "Colorado","MD": "Maryland","IA": "Iowa","OR": "Oregon","AK": "Alaska","NV": "Nevada","NY": "New York","DC": "District of Columbia","MN": "Minnesota","PA": "Pennsylvania","CT": "Connecticut","NJ": "New Jersey","CA" : "California"}
+allColumns = {
+	"higher": [("spending",2),("demographics",9),("eligibility",11),("takeup",13),("units",16),("payroll",20),("nonpayroll",21)],
+	"corrections": [("spending",2),("demographics",False),("eligibility",False),("takeup",13),("units",16),("payroll",20),("nonpayroll",21)],
+	"medicaid": [("spending",2),("demographics",9),("eligibility",11),("takeup",13),("spending",6)],
+	"ssi": [("spending",2),("demographics",9),("eligibility",11),("takeup",13),("spending",6)],
+	"tanf": [("spending",2),("demographics",9),("eligibility",11),("takeup",13),("spending",6)],
+	"ccdf": [("spending",2),("demographics",9),("eligibility",11),("takeup",13),("units",16),("spending",6)],
+	"housing": [("spending",2),("demographics",9),("eligibility",False),("takeup",13),("units",16),("payroll",20),("nonpayroll",21)],
+	# "admin": [("spending",2),("demographics",False),("eligibility",False),("takeup",False),("units",16),("payroll",20),("nonpayroll",21)],
+	"resources": [("spending",2),("demographics",False),("eligibility",False),("takeup",False),("units",16),("payroll",20),("nonpayroll",21)],
+	"parks": [("spending",2),("demographics",False),("eligibility",False),("takeup",False),("units",16),("payroll",20),("nonpayroll",21)],
+	"utilities": [("spending",2),("demographics",False),("eligibility",False),("takeup",False),("units",16),("payroll",20),("nonpayroll",21)],
+	"k12": [("spending",2),("demographics",9),("eligibility",11),("takeup",13),("units",16),("payroll",20),("nonpayroll",21)],
+	"fire": [("spending",2),("demographics",False),("eligibility",False),("takeup",False),("units",16),("payroll",20),("nonpayroll",21)],
+	"police": [("spending",2),("demographics",False),("eligibility",False),("takeup",13),("units",16),("payroll",20),("nonpayroll",21)],
+	"highway": [("spending",2),("demographics",9),("eligibility",11),("takeup",13),("units",16),("payroll",23),("nonpayroll",24)],
+	"transit": [("spending",2),("demographics",9),("eligibility",False),("takeup",13),("units",16),("payroll",23),("nonpayroll",24)]
+}
+
+def parseBook(category):
+	data = {}
+	cw = csv.writer(open("../data/%s.csv"%category,"wb"))
+	book = xlrd.open_workbook("%s/%s"%(BASE_PATH, FILENAMES[category]))
+	sheets = book.sheet_names()
+	xl_sheet = book.sheet_by_name(TABNAMES[category])
+	columns = allColumns[category]
+
+	for i in range(2, xl_sheet.nrows):
+		row = xl_sheet.row(i)
+		state = xl_sheet.cell_value(rowx=i, colx=0)
+		data[state] = {}
+		for c in columns:
+			if(c[1]):
+				value = xl_sheet.cell_value(rowx=i,colx=c[1])
+				data[state]["%s_value"%c[0]] = value
+				data[state]["%s_rank"%c[0]] = 0
+				if c[0] not in data:
+					data[c[0]] = []
+				data[c[0]].append((state, value))
+				data[c[0]] = sorted(data[c[0]],key=lambda x: x[1], reverse=True)
+	for state in data:
+		if state in STATES:
+			d = data[state]
+			for k in d:
+				if k.find("value") != -1:
+					cat = k.split("_")[0]
+					if d["%s_value"%cat] == "Missing Data":
+						d["%s_rank"%cat] = 99
+					else:
+						d["%s_rank"%cat] = collect(data[cat],0).index(state)+1
+
+	header = ["state"]
+	for c in columns:
+		header.append("%s_value"%c[0])
+		header.append("%s_rank"%c[0])
+	cw.writerow(header)
+	for state in STATES:
+		row = [state]
+		for c in columns:
+			if(c[1]):
+				row.append(data[state]["%s_value"%c[0]])
+				row.append(data[state]["%s_rank"%c[0]])
+			else:
+				row.append("")
+				row.append("")
+		cw.writerow(row)
+
+# parseBook("higher")
+# parseBook("corrections")
+for filename in FILENAMES:
+	parseBook(filename)
