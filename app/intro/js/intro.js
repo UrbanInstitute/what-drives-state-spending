bb7b537a1b926d244ebef737ffdae9b180709f8b
Ben Chartoff <bchartoff@gmail.com>
2016-01-28 16:06:35 -0500

    animations

--- app/intro/js/intro.js
+++ app/intro/js/intro.js
@@ -1,15 +1,28 @@
-var s1_done, s2_done;
+var s1_done, s2_done, s3_done, s4_done, s5_done;
 
 $(window).scroll(function(e){
 	var y = window.scrollY
 	console.log(y)
-	if(y >= 1979 && !s1_done){
+	if(y >= 1800 && !s1_done){
 		s1_done = true
 		step1();
 	}
-	else if(y >= 2777 && !s2_done){
+	else if(y >= 2700 && !s2_done){
 		s2_done = true
+		step1()
 		step2()
+	}
+	else if(y >= 3400 && !s3_done){
+		s3_done = true
+		step3()
+	}
+	else if(y >= 4250 && !s4_done){
+		s4_done = true
+		step4()
+	}
+	else if(y >= 5100 && !s5_done){
+		s5_done = true
+		step5()
 	}
 	if(y >= 2155){
 		fix();
@@ -34,21 +47,48 @@ function step1(){
 	fadeIn(".s3",1000,3000)
 }
 function step2(){
+	moveIn("#s1",0,0)
+	moveIn("#s2",0,0)
+	fadeIn(".s3",0,0)
+
 	moveIn("#s4",3000,0)
 	moveIn("#s5",3000,0)
 	fadeIn(".s6",1000,3000)
 	grey(".grey2")
 }
+function step3(){
+	moveIn("#s7",3000,0)
+	moveIn("#s8",3000,0)
+	fadeIn(".s9",1000,3000)
+	grey(".grey3")
+}
+function step4(){
+	moveIn("#s10",3000,0)
+	moveIn("#s11",3000,0)
+	fadeIn(".s12",1000,3000)
+	grey(".grey4")
+	white(".white4")
+}
+function step5(){
+	moveIn("#s13",3000,0)
+	moveIn("#s14",3000,0)
+	fadeIn(".s15",1000,3000)
+	grey(".grey5")
+	// white(".white4")
+}
 function moveIn(elem, duration, delay){
 	d3.select(elem)
-			.style("opacity",1)
-
+		.style("opacity",.5)
 		.transition()
 		.duration(duration)
 		.delay(delay)
 		.style("left","0px")
 		.style("top","0px")
 		.style("font-size","18px")
+		.style("opacity",1)
+		.each('end', function () {
+			// console.log(this)
+		})
 }
 function fadeIn(elem, duration, delay){
 	d3.selectAll(elem)
@@ -60,6 +100,12 @@ function fadeIn(elem, duration, delay){
 function grey(elem){
 	d3.selectAll(elem)
 		.transition()
-		.duration(2000)
+		.duration(1500)
 		.style("opacity",.3)
+}
+function white(elem){
+	d3.selectAll(elem)
+		.transition()
+		.duration(2000)
+		.style("opacity",1)
 }
