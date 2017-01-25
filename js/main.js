f1586d7bd064d29a059103e67766b4986cbfcd50
Ben Chartoff <bchartoff@gmail.com>
2017-01-25 16:15:50 -0500

    debug test

--- js/main.js
+++ js/main.js
@@ -1957,6 +1957,7 @@ $(window).scroll(scrollCheck);
 $(document).ready(function(){
 	d3.select("#navMenu select").node().value = "k12"
 	setTimeout(function(){
+		console.log("foo")
 		var rows = d3.selectAll(".row")[0]
 		if(rows.length > 52){
 			console.log("baleted")
@@ -1965,7 +1966,7 @@ $(document).ready(function(){
 			}
 			d3.select(".header").remove()
 		}
-	},1000);		
+	},6000);		
 	scrollCheck();
 });
 $(window).resize(function(e){
